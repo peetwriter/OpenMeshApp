b82b6fb8af24659d9c7a7b89804d0514a2e14503
peetwriter <petr@ubuntu.(none)>
2014-02-02 16:58:36 -0500

    mesh for convex hull

--- cube.cc
+++ cube.cc
@@ -1,5 +1,5 @@
-
 #include <iostream>
+// -------------------- OpenMesh
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 // ----------------------------------------------------------------------------
@@ -8,68 +8,45 @@ typedef OpenMesh::PolyMesh_ArrayKernelT<> MyMesh;
 // Build a simple cube and write it to std::cout
 int main()
 {
-MyMesh mesh;
-// generate vertices
-MyMesh::VertexHandle vhandle[8];
-vhandle[0] = mesh.add_vertex(MyMesh::Point(-1, -1, 1));
-vhandle[1] = mesh.add_vertex(MyMesh::Point( 1, -1, 1));
-vhandle[2] = mesh.add_vertex(MyMesh::Point( 1, 1, 1));
-vhandle[3] = mesh.add_vertex(MyMesh::Point(-1, 1, 1));
-vhandle[4] = mesh.add_vertex(MyMesh::Point(-1, -1, -1));
-vhandle[5] = mesh.add_vertex(MyMesh::Point( 1, -1, -1));
-vhandle[6] = mesh.add_vertex(MyMesh::Point( 1, 1, -1));
-vhandle[7] = mesh.add_vertex(MyMesh::Point(-1, 1, -1));
-// generate (quadrilateral) faces
-std::vector<MyMesh::VertexHandle> face_vhandles;
-face_vhandles.clear();
-face_vhandles.push_back(vhandle[0]);
-face_vhandles.push_back(vhandle[1]);
-face_vhandles.push_back(vhandle[2]);
-face_vhandles.push_back(vhandle[3]);
-mesh.add_face(face_vhandles);
-face_vhandles.clear();
-face_vhandles.push_back(vhandle[7]);
-face_vhandles.push_back(vhandle[6]);
-face_vhandles.push_back(vhandle[5]);
-face_vhandles.push_back(vhandle[4]);
-mesh.add_face(face_vhandles);
-face_vhandles.clear();
-face_vhandles.push_back(vhandle[1]);
-face_vhandles.push_back(vhandle[0]);
-face_vhandles.push_back(vhandle[4]);
-face_vhandles.push_back(vhandle[5]);
-mesh.add_face(face_vhandles);
-face_vhandles.clear();
-face_vhandles.push_back(vhandle[2]);
-face_vhandles.push_back(vhandle[1]);
-face_vhandles.push_back(vhandle[5]);
-face_vhandles.push_back(vhandle[6]);
-mesh.add_face(face_vhandles);
-face_vhandles.clear();
-face_vhandles.push_back(vhandle[3]);
-face_vhandles.push_back(vhandle[2]);
-face_vhandles.push_back(vhandle[6]);
-face_vhandles.push_back(vhandle[7]);
-mesh.add_face(face_vhandles);
-face_vhandles.clear();
-face_vhandles.push_back(vhandle[0]);
-face_vhandles.push_back(vhandle[3]);
-face_vhandles.push_back(vhandle[7]);
-face_vhandles.push_back(vhandle[4]);
-mesh.add_face(face_vhandles);
-// write mesh to output.obj
-try
-{
-if ( !OpenMesh::IO::write_mesh(mesh, "output.off") )
-{
-std::cerr << "Cannot write mesh to file 'output.off'" << std::endl;
-return 1;
-}
-}
-catch( std::exception& x )
-{
-std::cerr << x.what() << std::endl;
-return 1;
-}
-return 0;
+    MyMesh mesh;
+    // generate vertices
+
+    MyMesh::VertexHandle vhandle[8];
+
+   vhandle[0] = mesh.add_vertex(MyMesh::Point(-1, -1, 0));
+   vhandle[1] = mesh.add_vertex(MyMesh::Point( -1, 0, 0));
+   vhandle[2] = mesh.add_vertex(MyMesh::Point( -2, 1, 0));
+   vhandle[3] = mesh.add_vertex(MyMesh::Point(0, 1, 0));
+   vhandle[4] = mesh.add_vertex(MyMesh::Point(2, 2, 0));
+   vhandle[5] = mesh.add_vertex(MyMesh::Point( 1, 0, 0));
+   vhandle[6] = mesh.add_vertex(MyMesh::Point( 3, -1, 0));
+   vhandle[7] = mesh.add_vertex(MyMesh::Point( 0, -2, 0));;
+    // generate (quadrilateral) faces
+
+   std::vector<MyMesh::VertexHandle> face_vhandles;
+   face_vhandles.clear();
+   face_vhandles.push_back(vhandle[0]);
+   face_vhandles.push_back(vhandle[1]);
+   face_vhandles.push_back(vhandle[2]);
+   face_vhandles.push_back(vhandle[3]);
+   face_vhandles.push_back(vhandle[4]);
+   face_vhandles.push_back(vhandle[5]);
+   face_vhandles.push_back(vhandle[6]);
+   face_vhandles.push_back(vhandle[7]);
+   mesh.add_face(face_vhandles);
+
+    try
+    {
+        if ( !OpenMesh::IO::write_mesh(mesh, "outputnew.off") )
+        {
+            std::cerr << "Cannot write mesh to file 'output.off'" << std::endl;
+            return 1;
+        }
+    }
+    catch( std::exception& x )
+    {
+        std::cerr << x.what() << std::endl;
+        return 1;
+    }
+    return 0;
 }
